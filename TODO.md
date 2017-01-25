df0e9f8782bdb7e7187b5cd9c3bbefaf0750c801
Julie Alary <julie.alary@gmail.com>
2017-01-25 14:36:35 +0100

    reglage image

--- TODO.md
+++ TODO.md
@@ -9,7 +9,6 @@ _Mise en place de divers outils sans utiliser_
 
 - pb mofication et ajout d'image sous chrome est obligatoire alors que nedevrait pas : solution définir une image par défault??
 
-- probleme condition page 1 a régler au login et logout.
 
 - page de gestion des utils : 
 
--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new CMS\UserBundle\CMSUserBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Bmatzner\FontAwesomeBundle\BmatznerFontAwesomeBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
