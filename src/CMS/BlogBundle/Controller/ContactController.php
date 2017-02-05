33a2fdea3ead52816e9ab8a85f016234e9a05d00
JulieAlary <julie.alary@gmail.com>
2017-02-05 01:45:13 +0100

    formulaire font awesome enregistre!

--- src/CMS/BlogBundle/Controller/ContactController.php
+++ src/CMS/BlogBundle/Controller/ContactController.php
@@ -2,15 +2,54 @@
 
 namespace CMS\BlogBundle\Controller;
 
+use CMS\BlogBundle\Entity\Contact;
+use CMS\BlogBundle\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends Controller
 {
 
+    /**
+     * To display the main contact page admin
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction() {
 
         return $this->render(
           'CMSBlogBundle:Contact:index.html.twig'
         );
     }
+
+    public function fontAwesomeAction(Request $request) {
+
+        $contact_fa = new Contact();
+
+        $form = $this->get('form.factory')->create(ContactType::class, $contact_fa);
+
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($contact_fa);
+            $em->flush();
+
+            $request->getSession()->getFlashBag()->add('notice', 'Icône(s) bien enregistré(e)s.');
+
+            return $this->redirectToRoute(
+                'cms_contact_index'
+            );
+        }
+
+        return $this->render(
+          'CMSBlogBundle:Contact:addFontAwesome.html.twig',
+            [
+                'form' => $form->createView()
+            ]
+        );
+    }
 }
+
+
+
+
+
