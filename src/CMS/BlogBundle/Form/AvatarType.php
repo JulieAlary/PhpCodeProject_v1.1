c760478bdeef23d2cd767b1bf9b19ee62362e508
JulieAlary <julie.alary@gmail.com>
2017-02-15 17:24:16 +0100

    all views with avatar are ok!

---
+++ src/CMS/BlogBundle/Form/AvatarType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace CMS\BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AvatarType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('file', FileType::class, array(
+                'attr' =>array('class' =>'btn btn-file'),
+                'required' => false,
+                'label' => false
+            ));
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CMS\BlogBundle\Entity\Avatar'
+        ));
+    }
+}
