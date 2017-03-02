9501cab7184ca2bd607cb3d710f8955f4600867f
JulieAlary <julie.alary@gmail.com>
2017-03-02 15:28:38 +0100

    Quelques Tests Phpunit

---
+++ src/CMS/BlogBundle/Tests/Controller/BlogControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace CMS\BlogBundle\Tests\Controller;
+
+use CMS\BlogBundle\Entity\Category;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BlogControllerTest extends WebTestCase
+{
+
+    /**
+     * Vérifier si les pages se chargent
+     */
+    public function testPageIsSuccessful()
+    {
+
+        $client = static::createClient();
+
+        foreach ($this->provideUrls() as $url) {
+
+            $client->request('GET', $url);
+            $this->assertTrue($client->getResponse()->isSuccessful());
+        }
+    }
+
+    private function provideUrls()
+    {
+
+        return array('/');
+    }
+
+    /**
+     * Tester si on a bien des mots dans une page
+     */
+    public function testWordInpage()
+    {
+
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertEquals('CMS\CoreBundle\Controller\CoreController::indexAction', $client->getRequest()->attributes->get('_controller'));
+        $this->assertTrue(200 == $client->getResponse()->getStatusCode());
+        $this->assertTrue($crawler->filter('h3:contains("Actus")')->count() >= 1);
+    }
+
+}
